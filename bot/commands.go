1f29700992636f7a078a528696203a16ead3b854
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-05-24 21:03:20 +0330

    search & newest commands done

--- bot/commands.go
+++ bot/commands.go
@@ -1,6 +1,9 @@
 package bot
 
 import (
+	"fmt"
+	"strings"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/hosseinmirzapur/ecombot/database"
 	"github.com/hosseinmirzapur/ecombot/database/models"
@@ -63,31 +66,53 @@ func newestCommand(update tgbotapi.Update) {
 	var products []models.Product
 	chatID := update.Message.Chat.ID
 
-	err := database.DB().Select("title").Find(&products).Order("id DESC").Take(10).Error
+	err := database.DB().Select("title").Limit(10).Order("id DESC").Find(&products).Error
 	if err != nil {
 		handleErr(err, chatID)
 		return
 	}
 
-	var keyboard = tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonURL("back", "/start"),
-		),
-	)
-
-	for _, product := range products {
-		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData(product.Title, product.Title),
-		))
-
+	if len(products) == 0 {
+		handleBotMessage("محصولی در فروشگاه موجود نیست", chatID)
+		return
 	}
 
-	msg := tgbotapi.NewMessage(chatID, "newest products")
-	msg.ReplyMarkup = keyboard
-	sendToBot(msg)
+	productsKeyboard(products, chatID)
 }
 
-func searchCommand(update tgbotapi.Update) {}
+func searchCommand(update tgbotapi.Update) {
+	chatID := update.Message.Chat.ID
+
+	// extract search query after `/search` syntax
+	updateMsg := update.Message.Text
+	splitMsg := strings.Split(updateMsg, " ")
+	if len(splitMsg) < 2 {
+		handleErr(fmt.Errorf(`
+		برای جستجو در بین محصولات با فرمت زیر جستجو نمایید:
+
+		/search نام محصول
+
+		ابتدا عبارت search/ را قرار دهید و سپس نام محصول مورد نظر خود را وارد کنید تا جستجو صورت پذیرد
+		`), chatID)
+		return
+	}
+	searchQ := strings.Join(splitMsg[1:], ">")
+
+	var products []models.Product
+	err := database.DB().Where("title LIKE ?", "%"+searchQ+"%").Find(&products).Error
+	if err != nil {
+		handleErr(err, chatID)
+		return
+	}
+
+	if len(products) == 0 {
+		handleBotMessage("پس از جستجو موردی جهت نمایش یافت نشد", chatID)
+		return
+	}
+
+	productsKeyboard(products, chatID)
+
+}
 
 func helpCommand(update tgbotapi.Update) {}
 
--- bot/keyboards.go
+++ bot/keyboards.go
@@ -1,6 +1,9 @@
 package bot
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/hosseinmirzapur/ecombot/database/models"
+)
 
 func homeInlineKeyboard(chatID int64) {
 	keyboard := tgbotapi.NewInlineKeyboardMarkup(
@@ -22,4 +25,19 @@ func homeInlineKeyboard(chatID int64) {
 
 	sendToBot(msg)
 
+}
+
+func productsKeyboard(products []models.Product, chatID int64) {
+
+	var keyboard [][]tgbotapi.KeyboardButton
+
+	for _, product := range products {
+		keyboard = append(keyboard, tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton(product.Title),
+		))
+	}
+
+	msg := tgbotapi.NewMessage(chatID, "لیست محصولات")
+	msg.ReplyMarkup = tgbotapi.NewOneTimeReplyKeyboard(keyboard...)
+	sendToBot(msg)
 }
